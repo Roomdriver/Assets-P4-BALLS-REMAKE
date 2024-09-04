5687ed07a20edcbe5b4ad4f9265724891b85db0d
Roomdriver <gmroomdriver@gmail.com>
2024-09-04 05:34:36 +0200

    PlayerController

--- SCRIPTS/PlayerController.cs
+++ SCRIPTS/PlayerController.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{     
+{
+    public float speed=2;
+    Rigidbody rb;
+    public GameObject focalGameObject;
+    public Transform focal;
+
     void Start()
     {
-       
+        rb= GetComponent<Rigidbody>();
+        focal = focalGameObject.GetComponent<Transform>();  
     }
     
     void Update()
     {
-        
+        MovePlayer();
+        PlayerIsDead();
+    }
+    void MovePlayer()
+    {
+        float verticalForce = Input.GetAxis("Vertical");
+        float horizontalForce = Input.GetAxis("Horizontal");
+        rb.AddForce(focal.transform.forward* verticalForce * speed, ForceMode.Force);       
+        rb.AddForce(focal.transform.right * horizontalForce * speed, ForceMode.Force);
+    }
+    void PlayerIsDead()
+    {
+        if (transform.position.y<-3)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+            
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+            
     }
 }
