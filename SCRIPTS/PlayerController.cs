234698fde462a187a02e9dfd23cf786ab6a63ef5
Roomdriver <gmroomdriver@gmail.com>
2024-09-04 04:14:26 +0200

    Update PlayerController.cs

--- SCRIPTS/PlayerController.cs
+++ SCRIPTS/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     
     void Start()
     {
-        player.SetActive(false);
+        player.SetActive(true);
     }
 
     // Update is called once per frame
